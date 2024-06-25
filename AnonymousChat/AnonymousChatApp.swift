0b43b97d7e39412a4b59fbee58d343b0ba828ee0
Yamil Quispe <yamiljquispe@gmail.com>
2024-06-25 18:53:02 -0500

    Initial Commit

---
+++ AnonymousChat/AnonymousChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnonymousChatApp.swift
+//  AnonymousChat
+//
+//  Created by Yamil on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AnonymousChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
