677526db82e40103aad5fbb686935c863e225ec9
Yamil Quispe <yamiljquispe@gmail.com>
2024-07-30 14:05:12 -0500

    chat model branch

---
+++ AnonymousChat/Views/Chat/Models/Chat.swift
@@ -0,0 +1,60 @@
+//
+//  Chat.swift
+//  AnonymousChat
+//
+//  Created by Yamil on 7/29/24.
+//
+
+import Foundation
+
+struct Chat: Identifiable {
+    var id: String
+    var chatName: String?
+//    var lastMessage: [Message]
+    var creationDate: Date
+    var lastMessageTimeStamp: Date
+    var membersCount: UInt
+    var adminUids: [String]
+    var membersUids: [String]
+//    var members: [User]
+    var groupChatImage: String?
+    var groupChatDescription: String
+    var createdBy: String
+
+    var isGroupChat: Bool {
+        return membersCount > 2
+    }
+
+    static var placeholder = Chat(
+        id: "1",
+//        lastMessage: "Hello world",
+        creationDate: Date(),
+        lastMessageTimeStamp: Date(),
+        membersCount: 2,
+        adminUids: [],
+        membersUids: [],
+//        members: [],
+        groupChatDescription: "hola",
+        createdBy: "carlos"
+    )
+}
+
+
+extension Chat {
+    init(_ dict: [String: Any]) {
+        self.id = dict["_id"] as? String ?? ""
+        self.chatName = dict["name"] as? String ?? nil
+//        self.lastMessage = (dict["lastMessage"] as? [[String: Any]] ?? []).compactMap { Message($0) }
+        let creationInterval = dict["creationDate"] as? Double ?? 0
+        self.creationDate = Date(timeIntervalSince1970: creationInterval)
+        let lastMsgTimeStampInterval = dict["lastMessageTimeStamp"] as? Double ?? 0
+        self.lastMessageTimeStamp = Date(timeIntervalSince1970: lastMsgTimeStampInterval)
+        self.membersCount = dict["membersCount"] as? UInt ?? 0
+        self.adminUids = dict["adminUids"] as? [String] ?? []
+        self.membersUids = dict["membersUids"] as? [String] ?? []
+//        self.members = (dict["members"] as? [[String: Any]] ?? []).compactMap { User($0) }
+        self.groupChatImage = dict["thumbnailUrl"] as? String ?? nil
+        self.groupChatDescription = dict["groupChatDescription"] as? String ?? ""
+        self.createdBy = dict["createdBy"] as? String ?? ""
+    }
+}
