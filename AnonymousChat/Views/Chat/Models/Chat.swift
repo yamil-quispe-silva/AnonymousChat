5587c4354c8edcd011b89d243127f654da9f1e49
Yamil Quispe <yamiljquispe@gmail.com>
2024-08-02 17:51:28 -0500

    updated Chat model

--- AnonymousChat/Views/Chat/Models/Chat.swift
+++ AnonymousChat/Views/Chat/Models/Chat.swift
@@ -1,3 +1,4 @@
+
 //
 //  Chat.swift
 //  AnonymousChat
@@ -6,46 +7,47 @@
 //
 
 import Foundation
+import UIKit
 
 struct Chat: Identifiable {
     var id: String
     var chatName: String?
-//    var lastMessage: [Message]
+    //    var lastMessage: [Message]
+    var messages: [Message]
     var creationDate: Date
     var lastMessageTimeStamp: Date
     var membersCount: UInt
     var adminUids: [String]
     var membersUids: [String]
-//    var members: [User]
-    var groupChatImage: String?
+    var members: [User]
+    var groupChatImage: URL
     var groupChatDescription: String
     var createdBy: String
-
+    var friend: String
     var isGroupChat: Bool {
         return membersCount > 2
     }
-
     
-    static var placeholder = Chat(
-        id: "1",
-//        lastMessage: "Hello world",
-        creationDate: Date(),
-        lastMessageTimeStamp: Date(),
-        membersCount: 2,
-        adminUids: [],
-        membersUids: [],
-//        members: [],
-        groupChatDescription: "hola",
-        createdBy: "carlos"
-    )
+    
+    //    static var placeholder = Chat(
+    //        id: "1",
+    ////        lastMessage: "Hello world",
+    //        creationDate: Date(),
+    //        var messages: [Message()]
+    //        lastMessageTimeStamp: Date(),
+    //        membersCount: 2,
+    //        adminUids: [],
+    //        membersUids: [],
+    ////        members: [],
+    //        groupChatDescription: "hola",
+    //        createdBy: "carlos"
+    //    )
 }
-
 
 extension Chat {
     init(_ dict: [String: Any]) {
         self.id = dict["_id"] as? String ?? ""
         self.chatName = dict["name"] as? String ?? nil
-//        self.lastMessage = (dict["lastMessage"] as? [[String: Any]] ?? []).compactMap { Message($0) }
         let creationInterval = dict["creationDate"] as? Double ?? 0
         self.creationDate = Date(timeIntervalSince1970: creationInterval)
         let lastMsgTimeStampInterval = dict["lastMessageTimeStamp"] as? Double ?? 0
@@ -53,18 +55,32 @@ extension Chat {
         self.membersCount = dict["membersCount"] as? UInt ?? 0
         self.adminUids = dict["adminUids"] as? [String] ?? []
         self.membersUids = dict["membersUids"] as? [String] ?? []
-//        self.members = (dict["members"] as? [[String: Any]] ?? []).compactMap { User($0) }
-        self.groupChatImage = dict["thumbnailUrl"] as? String ?? nil
+        // Convert members dictionary to User array
+        self.members = (dict["members"] as? [[String: Any]] ?? []).compactMap { User($0) }
+          // Convert messages dictionary to Message array
+        self.messages = (dict["messages"] as? [[String: Any]] ?? []).compactMap { Message($0) }
+        // Convert groupChatImage to URL, using a default URL if necessary
+        if let imageString = dict["thumbnailUrl"] as? String, let url = URL(string: imageString) {
+            self.groupChatImage = url
+        } else {
+            self.groupChatImage = URL(string: "<https://default.com/default.png>")!
+        }
         self.groupChatDescription = dict["groupChatDescription"] as? String ?? ""
         self.createdBy = dict["createdBy"] as? String ?? ""
+        self.friend = dict["friend"] as? String ?? ""
     }
+                  
 }
+                  
 
 
 struct User: Identifiable {
     let id: UUID
     let name: String
+    var image: UIImage?
+    var social: String?
 }
+      
 
 struct Message: Identifiable {
     let id: UUID
@@ -72,3 +88,25 @@ struct Message: Identifiable {
     let content: String
     let timestamp: Date
 }
+
+
+extension User {
+    init(_ dict: [String: Any]) {
+        self.id = UUID(uuidString: dict["id"] as? String ?? "") ?? UUID()
+        self.name = dict["firstName"] as? String ?? ""
+        self.image = dict["image"] as? UIImage
+        self.social = dict["social"] as? String
+    }
+}
+
+
+extension Message {
+    init(_ dict: [String: Any]) {
+        self.id = UUID(uuidString: dict["id"] as? String ?? "") ?? UUID()
+        self.sender = User(dict["sender"] as? [String: Any] ?? [:])
+        self.content = dict["content"] as? String ?? ""
+        let timestampInterval = dict["timestamp"] as? Double ?? 0
+        self.timestamp = Date(timeIntervalSince1970: timestampInterval)
+    }
+}
+
